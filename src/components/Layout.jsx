e93a2440ee445eb2041ccaccc19fc7b4e6ed71c1
Nick YOOO <outgrowthr@gmail.com>
2023-06-23 01:39:30 +0900

    Feat: redux 적용

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { styled } from "styled-components";
 
 const Layout = (props) => {
-    return (
-        <StLayout>{props.children}</StLayout>
-    )
+  return (
+    <StLayout>{props.children}</StLayout>
+  )
 }
 export default Layout
 
